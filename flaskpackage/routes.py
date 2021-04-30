50b417c69a3976eaef1bf049095b99b65fe8a046
Aleksander Sadowski <47467484+alekssadowski95@users.noreply.github.com>
2021-04-30 22:19:03 +0200

    asdasd

--- flaskpackage/routes.py
+++ flaskpackage/routes.py
@@ -5,18 +5,7 @@ from flaskpackage.forms import AddNoteForm
 from sqlalchemy import desc
 
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    # get all notes from the database and order by newest ones coming first
-    notes = Note.query.order_by(desc(Note.id)).all()
-    return render_template('home.html', notes=notes)
+    return render_template('home.html')
 
-@app.route('/new', methods=['GET', 'POST'])
-def add_note():
-    form = AddNoteForm()
-    if form.validate_on_submit():
-        note = Note(title=form.title.data, text=form.text.data)
-        db.session.add(note)
-        db.session.commit()
-        return redirect(url_for('home'))
-    return render_template('add-note.html', title='New note', form=form)
